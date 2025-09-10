21335cdf67a65876fd341a2202fc9c987c96837a
fishofeyes <774249737@qq.com>
2025-09-10 18:13:23 +0800

    user

--- lib/out/view/out_user_cover.dart
+++ lib/out/view/out_user_cover.dart
@@ -7,34 +7,32 @@ class OutUserCover extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Image.asset("assets/user.png", width: 52, height: 52),
-        ClipRRect(
-          clipBehavior: Clip.antiAlias,
-          borderRadius: BorderRadius.circular(25),
-          child: ExtendedImage.network(
-            url ?? '',
-            width: 50,
-            height: 50,
-            cacheHeight: 100,
-            cacheWidth: 100,
-            fit: BoxFit.cover,
-            loadStateChanged: (state) {
-              if (state.extendedImageLoadState == LoadState.failed ||
-                  state.extendedImageLoadState == LoadState.loading) {
-                return const SizedBox();
-              } else {
-                return ExtendedRawImage(
-                  image: state.extendedImageInfo?.image,
-                  fit: BoxFit.cover,
-                );
-              }
-            },
-          ),
+    return SizedBox(
+      width: 52,
+      height: 52,
+      child: ClipRRect(
+        clipBehavior: Clip.antiAlias,
+        borderRadius: BorderRadius.circular(25),
+        child: ExtendedImage.network(
+          url ?? '',
+          width: 50,
+          height: 50,
+          cacheHeight: 100,
+          cacheWidth: 100,
+          fit: BoxFit.cover,
+          loadStateChanged: (state) {
+            if (state.extendedImageLoadState == LoadState.failed ||
+                state.extendedImageLoadState == LoadState.loading) {
+              return Image.asset("assets/user.png", width: 52, height: 52);
+            } else {
+              return ExtendedRawImage(
+                image: state.extendedImageInfo?.image,
+                fit: BoxFit.cover,
+              );
+            }
+          },
         ),
-      ],
+      ),
     );
   }
 }
