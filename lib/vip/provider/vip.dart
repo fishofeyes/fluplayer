626012a08ba4a743c3c0086786d388e0e3943762
fishofeyes <774249737@qq.com>
2025-11-26 17:10:53 +0800

    break

--- lib/vip/provider/vip.dart
+++ lib/vip/provider/vip.dart
@@ -45,6 +45,7 @@ class Vip extends _$Vip {
       final status = tranc.status;
       switch (status) {
         case PurchaseStatus.pending:
+          break;
         case PurchaseStatus.purchased:
           await _buyRequest(tranc, true);
           EasyLoading.dismiss();
