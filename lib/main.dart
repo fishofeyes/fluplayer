af198a857209694a03bcebdb6341d9623ca8d151
fishofeyes <774249737@qq.com>
2026-08-20 18:14:46 +0800

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'common/common_app.dart';
 import 'common/common_report/common_report.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   CommonReport.uniqueId();
   HttpHelper.log();
   await CommonApp.init();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyAyV96So8i4mPw30wRwZWlanrlrjj_YToE",
+      appId: "1:1026087443559:ios:d38df4bcdb818a1786c8c2",
+      messagingSenderId: "1026087443559",
+      projectId: "fluplayer-ios",
+    ),
+  );
   await CommonHive.init();
   await admobHelper.init();
 
